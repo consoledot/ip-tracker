b12332de680a64900ac757a2aa7096cb8f0b9d7b
bimbooladele@outlook.com <abimbola.oladele@thebulb.africa>
2020-12-28 10:55:09 +0100

    updated map section

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,8 +3,7 @@ interface MapProps{
   coordinate:number[]
   name:string
 }
-export const Map = ({name,coordinate}:MapProps)=>
-coordinate ? (
+export const Map = ({name,coordinate}:MapProps)=>(
    <MapContainer center={[coordinate[0],coordinate[1]]} zoom={13} scrollWheelZoom={false} style={{ width: '100%', height: '60vh',zIndex:100}}>
     <TileLayer
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -17,4 +16,4 @@ coordinate ? (
     </Marker>
   </MapContainer> 
 
-) : <h1>Loading</h1>
+) 
